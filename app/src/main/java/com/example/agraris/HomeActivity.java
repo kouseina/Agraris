327bced10077cb555e6deadc4726300fa500469b
kouseina <56005648+kouseina@users.noreply.github.com>
2020-03-26 14:12:37 +0700

    add login with firebase

--- app/src/main/java/com/example/agraris/HomeActivity.java
+++ app/src/main/java/com/example/agraris/HomeActivity.java
@@ -2,13 +2,30 @@ package com.example.agraris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private  FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intoMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intoMain);
+            }
+        });
     }
 }
